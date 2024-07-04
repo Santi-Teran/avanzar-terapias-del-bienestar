14ac764599e5312c8793d7030e184d7fcf2e91c4
Santi-Teran <santiagoteranmatias@gmail.com>
2024-07-04 10:16:30 -0300

    modularizacion

--- src/api/handleBuy.js
+++ src/api/handleBuy.js
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
+import { getProductById } from './api';
 
-export const createPreference = async (product) => {
+const createPreference = async (product) => {
   try {
     const response = await axios.post(
       `https://avanzarbackend.azurewebsites.net/api/MercadoPago/create?amount=${product.price}&productName=${encodeURIComponent(product.name)}`
@@ -10,4 +11,33 @@ export const createPreference = async (product) => {
   } catch (error) {
     console.error('Error creating preference:', error);
   }
+};
+
+export const handleBuy = async (product, setPreferenceId, setIsLoading) => {
+  if (product) {
+    setIsLoading(true); // Mostrar indicador de carga
+    const preferenceId = await createPreference(product);
+    if (preferenceId) {
+      setPreferenceId(preferenceId);
+    }
+    setIsLoading(false); // Ocultar indicador de carga
+  }
+};
+
+export const fetchProductDetail = async (id) => {
+  if (id) {
+    const productId = parseInt(id, 10);
+    if (isNaN(productId)) {
+      console.error('Invalid product ID');
+      return null;
+    }
+    try {
+      const productDetail = await getProductById(productId);
+      return productDetail;
+    } catch (error) {
+      console.error('Error fetching product:', error);
+      return null;
+    }
+  }
+  return null;
 };
--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,10 +1,9 @@
 'use client';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { createPreference } from '@/api/handleBuy';
+import { fetchProductDetail, handleBuy } from '@/api/handleBuy';
 import NavBar from '@/components/Navbar';
 import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
-import { getProductById } from '@/api/api';
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -12,48 +11,21 @@ const ProductDetail = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  initMercadoPago("TEST-91aee071-1995-40d7-b374-a92c6cae0698", {
+  initMercadoPago(process.env.NEXT_PUBLIC_PUBLIC_KEY, {
     locale: "es-AR"
   });
 
-  const handleBuy = async () => {
-    if (product) {
-      setIsLoading(true); // Mostrar indicador de carga
-      const preferenceId = await createPreference(product);
-      if (preferenceId) {
-        setPreferenceId(preferenceId);
-      }
-      setIsLoading(false); // Ocultar indicador de carga
-    }
-  };
-
   useEffect(() => {
-    const fetchProduct = async () => {
-      if (id) {
-        const productId = parseInt(id, 10);
-        if (isNaN(productId)) {
-          console.error('Invalid product ID');
-          return;
-        }
-        let productDetail = null;
-        const storedProducts = localStorage.getItem('products');
-        if (storedProducts) {
-          const products = JSON.parse(storedProducts);
-          productDetail = products.find(product => product.id === productId);
-        }
-        if (!productDetail) {
-          // Si no se encuentra en localStorage, buscar en la API
-          productDetail = await getProductById(productId);
-        }
-        if (productDetail) {
-          setProduct(productDetail);
-        } else {
-          console.error('Product not found');
-        }
+    const getProduct = async () => {
+      const productDetail = await fetchProductDetail(id);
+      if (productDetail) {
+        setProduct(productDetail);
+      } else {
+        console.error('Product not found');
       }
     };
 
-    fetchProduct();
+    getProduct();
   }, [id]);
 
   if (!product) return <div>Loading...</div>;
@@ -70,7 +42,7 @@ const ProductDetail = () => {
           <p>{product.description}</p>
         </div>
         <div className='md:w-1/2 h-80 bg-slate-400 mb-14 md:mb-0'>
-          <button onClick={handleBuy} className='border' disabled={isLoading}>
+          <button onClick={() => handleBuy(product, setPreferenceId, setIsLoading)} className='border' disabled={isLoading}>
             {isLoading ? 'Cargando...' : 'Comprar'}
           </button>
           {preferenceId && <Wallet initialization={{ preferenceId }} customization={{ texts: { valueProp: 'smart_option' } }} />}
